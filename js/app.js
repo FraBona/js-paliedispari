7fce50d4b6d5fdea737500b0ee39f7e452f7a0b9
FraBona <francescobonafede08@gmail.com>
2023-10-12 16:24:37 +0200

    paridispari

--- js/app.js
+++ js/app.js
@@ -20,11 +20,36 @@ palindroma(stringa);
 
 /* PARI O DISPARI */
 function computer(n){
-   n = Math.floor(Math.random() * 5);
+   n = Math.floor(Math.random() * 5) + 1;
 
   return n;
 }
-let ncomputer = computer(n);
+
+function vincitore(n1, n2, utente){
+  let result = n1 + n2;
+  console.log(result);
+
+  if((result % 2) == 0){
+    if(utente == 'pari'){
+      console.log('hai vinto');
+    }
+    else if(utente == 'dispari'){
+      console.log('hai perso');
+    }
+  }
+  else{
+    if(utente == 'dispari'){
+      console.log('hai vinto');
+    }
+    else if(utente == 'pari'){
+      console.log('hai perso');
+    }
+  }
+}
+let ncomputer = computer();
 console.log(ncomputer);
 let user = prompt('scegli tra pari o dispari');
-let number = prompt('scegli un numero tra 1 e 5');
+let number = parseInt(prompt('scegli un numero tra 1 e 5'));
+console.log(number);
+
+vincitore(ncomputer, number, user);
